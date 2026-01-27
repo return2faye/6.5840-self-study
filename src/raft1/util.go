a174fe0463c9b4d5bb3f167aadf363ede1eb8267
return2faye <chongzhenma@gmail.com>
2026-01-27 23:14:37 +0800

    feat: Generate New rand Election Timeout

--- src/raft1/util.go
+++ src/raft1/util.go
@@ -1,12 +1,20 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
 const (
 	FOLLOWER = iota
 	CANDIDATE
 	LEADER
 )
+
+func NewElectionTimeOut() time.Duration {
+	return time.Duration(300 + rand.Intn(100)) * time.Millisecond
+}
 
 // Debugging
 const Debug = false
